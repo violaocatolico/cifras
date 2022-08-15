b46eaee7a4c1d1875e22ffd0603313e0a4b58224
Antônio Oliveira <antonio.filho128@gmail.com>
2022-08-15 10:29:28 -0300

    Add analytics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import ReactGA from 'react-ga';
 import Section1 from './app/sections/Section1';
 import Section2 from './app/sections/Section2';
 import Section3 from './app/sections/Section3';
 import Section4 from './app/sections/Section4';
 import Section5 from './app/sections/Section5';
 import Footer from './app/sections/Footer';
+
+ReactGA.initialize('G-QJHP22L4RH', {
+  // debug: true,
+  // gaOptions: {
+  //   userId: 123
+  // }
+});
+
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App() {
 
