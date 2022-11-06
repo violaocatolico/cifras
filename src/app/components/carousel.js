05bd0d0a610da32c38d1e2c6a9704060bc237192
Antônio Oliveira <antonio.filho128@gmail.com>
2022-11-06 01:09:08 -0300

    Checkpoint

--- src/app/components/carousel.js
+++ src/app/components/carousel.js
@@ -1,12 +1,15 @@
 import React, { useState, useLayoutEffect, useRef } from "react";
 import styled from 'styled-components';
 
+const shownItems = 3;
+const animTime = 0.5;
 
 const PageExample = styled.div`
   position: relative;
-  width: 100%;
-  height: 50vh;
-  border: solid 1px black;
+  margin: 0 auto;
+  width: 400px;
+  height: 200px;
+  background-color: orange;
 `;
 
 const Container = styled.div`
@@ -20,7 +23,9 @@ const SlidersContainer = styled.div`
   align-items: center;
   width: 100%;
   height: 100%;
-  overflow: hidden;
+  
+  /* trocar para hidden */
+  overflow: visible;
   position: relative;
 `;
 
@@ -31,10 +36,6 @@ const SlidersTrack = styled.div`
   height: 100%;
   overflow: visible;
   position: absolute;
-  transition: left .5s;
-  
-  /* Altera a posição do sliders tracker */
-  left: ${props => props.slidersTrackCssLeft}px;
 `;
 
 const SlidePage = styled.div`
@@ -44,17 +45,22 @@ const SlidePage = styled.div`
   width: ${props => props.pageSize}px;
   height: 80%;
   margin: ${props => props.marginSize}px ${props => props.marginSize}px;
-  transition: height .5s;
+  transition: height ${() => `${animTime}`}s, left ${() => `${animTime}`}s, opacity 0.5s;
 
   &.currentCenterPage {
     height: 100%;
   }
+
+  /* Altera a posição */
+  left: ${props => props.pageCss.left}px;
+  opacity: ${props => props.pageCss.opacity};
+  position: absolute;
 `;
 
 const Arrow = styled.a`
+  z-index: 99;
   width: 50px;
   height: 50px;
-  background-color: blue;
   background-size: contain;
   background-repeat: no-repeat;
   background-position: center;
@@ -72,7 +78,7 @@ const Carousel = () => {
   const slidersContainerRef = useRef(null);
 
   // Começa com a segunda página do array centralizada
-  const [currentCenterPage, setCurrentCenterPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState(0);
 
   const [dimensions, setDimensions] = useState({
     containerWidth: 0, 
@@ -95,46 +101,73 @@ const Carousel = () => {
   const marginSize = containerSize * 0.02;
   const pageSize = (containerSize / 3) - (marginSize * 2);
 
-  const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const [array, setArray] = useState([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
 
   return (
     <PageExample>
       <Container>
         <ArrowLeft href="#" onClick={(e) => {
           e.preventDefault();
-          setCurrentCenterPage(currentCenterPage - 1);
+
+          if (currentPage === 0) {
+            setCurrentPage(4);
+          } else {
+            setCurrentPage(currentPage - 1);
+          }
+
         }} />
         <SlidersContainer ref={slidersContainerRef} dimensions={dimensions}>
-          <SlidersTrack slidersTrackCssLeft={getSlidersTrackCssLeft(currentCenterPage, (containerSize / 3))}>
-            {
-              array.map((el, index) => (
-                <SlidePage 
-                  className={index === currentCenterPage ? "currentCenterPage" : ""}
-                  key={index}
-                  pageSize={pageSize}
-                  marginSize={marginSize}
-                />
-              ))
-            }
+          <SlidersTrack>
+
+            <SlidePage pageCss={pageCss(0, currentPage, (containerSize / 3))} key={0} pageSize={pageSize} marginSize={marginSize}>{array[0]}</SlidePage>
+            <SlidePage pageCss={pageCss(1, currentPage, (containerSize / 3))} key={1} pageSize={pageSize} marginSize={marginSize}>{array[1]}</SlidePage>
+            <SlidePage pageCss={pageCss(2, currentPage, (containerSize / 3))} key={2} pageSize={pageSize} marginSize={marginSize}>{array[2]}</SlidePage>
+            <SlidePage pageCss={pageCss(3, currentPage, (containerSize / 3))} key={3} pageSize={pageSize} marginSize={marginSize}>{array[3]}</SlidePage>
+            <SlidePage pageCss={pageCss(4, currentPage, (containerSize / 3))} key={4} pageSize={pageSize} marginSize={marginSize}>{array[4]}</SlidePage>
+
+
           </SlidersTrack>
         </SlidersContainer>
         <ArrowRight href="#" onClick={(e) => {
           e.preventDefault();
-          setCurrentCenterPage(currentCenterPage + 1);
+
+          if (currentPage === 4) {
+            setCurrentPage(0);
+          } else {
+            setCurrentPage(currentPage + 1);
+          }
         }} />
       </Container>
-      
-      <br /><br /><br /><br />
-      {JSON.stringify(dimensions)}
 
       <br /><br />
-      { `currentCenterPage: ${currentCenterPage}` }
+      { `currentPage: ${currentPage}` }
+
+      <br /><br />
+      { `array: ${array}` }
+      
     </PageExample>
   );
 }
 
-const getSlidersTrackCssLeft = (currentCenterPage, pageSize) => {
-  return ((currentCenterPage * pageSize) * -1) + pageSize;
+const getPosition = (currentPage, totalItems, thisPage) => {
+  return (currentPage + totalItems + thisPage) % totalItems;
+}
+
+const getLeftByPosition = (position, pageSize) => {
+  return (pageSize * position) - pageSize;
+}
+
+const pageCss = (thisPage, currentPage, pageSize) => {
+  const leftRightAuxItems = 2;
+  const totalItems = shownItems + leftRightAuxItems;
+
+  let position = getPosition(currentPage, totalItems, thisPage);
+  let left = getLeftByPosition(position, pageSize);
+
+  return {
+    left: left,
+    opacity: (position === 0 || position === 4) ? 0.5 : 1
+  };
 }
 
 export default Carousel;
